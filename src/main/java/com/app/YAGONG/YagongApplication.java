02056826032bbb1265945a3177d220e1f4fb9c20
JoHyunwoo <jhw3874@gmail.com>
2025-07-06 21:50:31 +0900

    git setting
    - .gitignore 설정

---
+++ src/main/java/com/app/YAGONG/YagongApplication.java
@@ -0,0 +1,13 @@
+package com.app.YAGONG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YagongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/YAGONG/YagongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.YAGONG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YagongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
